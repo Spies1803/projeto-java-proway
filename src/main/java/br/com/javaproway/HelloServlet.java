28bc6288e729a9c537fac4ef6c1df6e58443ee4a
Spies1803 <tpspies@gmail.com>
2019-10-20 19:03:28 -0300

    pagina HelloServlet

---
+++ src/main/java/br/com/javaproway/HelloServlet.java
@@ -0,0 +1,29 @@
+package br.com.javaproway;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/oi")
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<Primeiro Servlet HelloWorld javaweb>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
